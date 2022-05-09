8efd7f873024e63bdde7980e4730bb6de135d17a
Maiten Cullen <maitencullen@gmail.com>
2022-05-08 21:25:57 -0300

    ultimos cambios entregados

---
+++ mijava.js
@@ -0,0 +1,9 @@
+let usuario = prompt("ingresar nombre usuario");
+let contraseña = prompt("ingresar contraseña");
+let programa = prompt("ya sos parte del programa?");
+
+if ((usuario !="") && (contraseña !="") && (programa = "si")){
+    alert("bienvenido");
+} else if (usuario = ""){
+    alert ("error, solor pueen ingresar miembros del programa");
+}
